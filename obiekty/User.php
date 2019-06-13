f6bde4e1a8d05c9b5e42b9b1dfe17591254cb3ee
Filip Czapliński <filuuuus@gmail.com>
2019-06-14 00:25:12 +0200

    logowanie urzytkowników z bazy

--- obiekty/User.php
+++ obiekty/User.php
@@ -2,7 +2,7 @@
 require_once('Database.php');
 class User
 {
-    protected $id_user;
+    protected $user_id;
     protected $login;
     protected $email;
     protected $name;
@@ -13,6 +13,14 @@ class User
     {
         $this->conect = new Database;
         $this->conect->conect();
+        if(isset($_SESSION['user'])) {
+            $this->user_id = $_SESSION['user']['user_id'];
+            $this->login = $_SESSION['user']['login'];
+            $this->email = $_SESSION['user']['email'];
+            $this->name = $_SESSION['user']['name'];
+            $this->surname = $_SESSION['user']['surname'];
+            $this->password = $_SESSION['user']['password'];
+        }
     }
     public function __destruct()
     {
@@ -20,19 +28,33 @@ class User
     }
     public function login($login, $password)
     {
-        if($login == 'admin' && $password == 'admin') {
-            $this->id_user = 1;
-            $this->login = 'admin';
-            $this->email = 'admin@admin.pl';
-            $this->name = 'Jan';
-            $this->surname = 'Kowalski';
-            $this->password = 'admin';
-            $this->createInSession();
+        $query = "  SELECT * FROM myusers
+                    WHERE
+                    login = '$login'";
+        $result = mysqli_query($this->conect->getHook(), $query) or die(mysql_error());
+        if(mysqli_num_rows($result) > 0) {
+            $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if($login == $record['login'] && $password == $record['password']) {
+                $this->user_id = $record['user_id'];
+                $this->login = $record['login'];
+                $this->email = $record['email'];
+                $this->name = $record['name'];
+                $this->surname = $record['surname'];
+                $this->password = $record['password'];
+                $this->createInSession();
+            }
+            else {
+                header("Location: index.php");
+            }
         }
+        else {
+            header("Location: index.php");
+        }
+        
     }
     public function createInSession()
     {
-        $_SESSION['user']['id_user'] = $this->id_user;
+        $_SESSION['user']['user_id'] = $this->user_id;
         $_SESSION['user']['login'] = $this->login;
         $_SESSION['user']['email'] = $this->email;
         $_SESSION['user']['name'] = $this->name;
@@ -47,11 +69,11 @@ class User
     }
     public function updateUser($login, $email, $name, $surname)
     {
-        $id_user = $_SESSION['user']['id_user'];
+        $user_id = $_SESSION['user']['user_id'];
         $query = "  UPDATE myusers SET
                     login = '$login', email = '$email', name = '$name', surname = '$surname'
                     WHERE
-                    id_user = $id_user";
+                    user_id = $user_id";
         mysqli_query($this->conect->getHook(), $query) or die(mysqli_error($this->conect->getHook()));
     }
 }
